dce64da1ed4a0b4a30481a831e4dcf50d45a097a
AndriBunko <andr1bunk02@gmail.com>
2019-10-22 10:36:34 +0300

    part 3, leson 22 passing parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учю компоненты', id: 1},
+    {title: 'Следущий блок', text: 'Будет про директивы и пайпы', id: 2}
+  ];
 }
