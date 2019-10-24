76abfc2eeb13fdef653be26a0f724617df0e460d
AndriBunko <andr1bunk02@gmail.com>
2019-10-24 10:51:30 +0300

    part 3, lesson 28 detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {filter} from 'rxjs/operators';
 
 export interface Post {
@@ -12,10 +12,10 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   posts: Post[] = [
     {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учю компоненты', id: 1},
-    {title: 'Следущий блок', text: 'Будет про директивы и пайпы', id: 2}
+    // {title: 'Следущий блок', text: 'Будет про директивы и пайпы', id: 2}
   ];
 
   updatePost(post: Post) {
@@ -26,5 +26,16 @@ export class AppComponent {
   removePost(id: number) {
     console.log('Id', id);
     this.posts = this.posts.filter(p => p.id !== id);
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Timout');
+      this.posts[0] = {
+        title: 'Changded',
+        text: 'Changed2',
+        id: 33
+      };
+    }, 5000);
   }
 }
